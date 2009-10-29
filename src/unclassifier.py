0d80167d8617c6802d9825e109d036a37b0970c0
Marko Ivankovic <ivankovic.42@gmail.com>
2009-10-29 08:42:35 +0100

    Even better testing framework!

--- src/unclassifier.py
+++ src/unclassifier.py
@@ -2,18 +2,24 @@
 
 from classifier import Classifier
 import Image
+import random
 
 class Unclassifier(Classifier):
 	name = "Bles-o-mat"
+	ids = []
 
 	def train(self, traindata):
 		""" 	Traindata sadrzi dictionary u kojem su kljucevi ID
 			faca a vrijednosti liste Image objekata u kojima su slikice """
-		pass
+		for x in traindata:
+			for y in traindata[x]:
+				# do stuff...
+				pass
+			self.ids.append(x)
 
 	def classify(self, image):
 		""" Image objekt, return mora biti tocan ID osobe """
-		return 0
+		return random.choice(self.ids)
 
 
 instance = Unclassifier()
