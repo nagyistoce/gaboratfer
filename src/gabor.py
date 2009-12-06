cbae931c4f8d878d3357478b61652553d357a201
ivan.kristo <ivan.kristo@localhost>
2009-12-06 01:44:37 +0100

    Ubrzao konvoluciju. Sve isprobane metode su u kodu.

--- src/gabor.py
+++ src/gabor.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import numpy
+from numpy.fft import fft2, ifft2
 import math
 from PIL import Image
+from scipy import signal
 
 def gaborFunction(x, y, Lambda, theta, psi, sigma, gamma):
 	"""	Gaborova funkcija.
@@ -45,8 +47,83 @@ def gaborFilterImage(height, width, minx, miny, maxx, maxy, Lambda, theta, psi, 
 
 def degToRad(deg):
 	return 2 * math.pi * deg / 360.0
+
+from scipy.stsci import convolve
+
+def applyScipyConv(filter, image):
+	r2 = image.size[0]
+	c2 = image.size[1]
+	r1 = len(filter)
+	c1 = len(filter[0])
+	image.shape = (r2, c2)
+	image.dtype = numpy.uint8
+	filter.shape = (r1, c1)
+	result = signal.fftconvolve(image, filter)
+	#result = signal.correlate(image, filter)
+	#result = convolve.convolve2d(filter, image, fft=1)
+
+	resultImage = Image.new("L", (r2, c2))
+	resultCanvas = resultImage.load()
+	valueMax = result.max()
+	valueMin = result.min()
 	
-def apply(filter, image):
+	for x in xrange(0, r2):
+		for y in xrange(0, c2):
+			resultCanvas[y, x] = (result[x, y] - valueMin) * 255 / (valueMax - valueMin)
+	return resultImage
+
+def apply(filter, image, MinPad=True, pad=True):
+	""" Not so simple convolution """
+
+	#Just for comfort:
+	FFt = fft2
+	iFFt = ifft2
+
+	#The size of the images:
+	r2 = image.size[0]
+	c2 = image.size[1]
+	r1 = len(filter)
+	c1 = len(filter[0])
+	
+	#MinPad results simpler padding,smaller images:
+	if MinPad:
+		r = r1+r2
+		c = c1+c2
+	else:
+		#if the Numerical Recipies says so:
+		r = 2*max(r1,r2)
+		c = 2*max(c1,c2)
+
+	#For nice FFT, we need the power of 2:
+	if pad:
+		pr2 = int(math.log(r)/math.log(2.0) + 1.0 )
+		pc2 = int(math.log(c)/math.log(2.0) + 1.0 )
+		rOrig = r
+		cOrig = c
+		r = 2**pr2
+		c = 2**pc2
+	#end of if pad
+	
+	#numpy fft has the padding built in, which can save us some steps
+	#here. The thing is the s(hape) parameter:
+	fftimage = FFt(filter, s=(r,c))*FFt(image,s=(r,c))
+
+	if pad:
+		result = (iFFt(fftimage))[:rOrig,:cOrig].real
+	else:
+		result = (iFFt(fftimage)).real
+
+	resultImage = Image.new("L", (r2, c2))
+	resultCanvas = resultImage.load()
+	valueMax = result.max()
+	valueMin = result.min()
+	
+	for x in xrange(0, r2):
+		for y in xrange(0, c2):
+			resultCanvas[y, x] = (result[x, y] - valueMin) * 255 / (valueMax - valueMin)
+	return resultImage
+
+def applyDirectConvolution(filter, image):
 	imageHeight = image.size[0]
 	imageWidth = image.size[1]
 	filterHeight = len(filter)
@@ -68,6 +145,7 @@ def apply(filter, image):
 	resultCanvas = resultImage.load()
 	valueMax = result.max()
 	valueMin = result.min()
+	
 	for x in xrange(0, imageHeight):
 		for y in xrange(0, imageWidth):
 			resultCanvas[x, y] = (result[x, y] - valueMin) * 255 / (valueMax - valueMin)
@@ -83,5 +161,8 @@ if __name__ == "__main__":
 	filtr = gaborFilter(8, 8, -4, -4, 4, 4, 2, 0, 0, 1, 1)
 
 	img = readImage("./../data/000_2_1.nrm")
+	#result = applyDirectConvolution(filtr, img)
 	result = apply(filtr, img)
+	#result = applyScipyConv(filtr, img)
+
 	result.save("test.png")
