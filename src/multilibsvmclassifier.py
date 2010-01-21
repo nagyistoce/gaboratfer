ec0e06ff0f46ae1ab20658d2c4b8b77618269d4f
mateja <mateja@mateja-laptop>
2010-01-21 17:42:46 +0100

    Sredjeni klasifikatori za evaluaciju (onaj multi class...). Broj test uzoraka stavljen na 3.

--- src/multilibsvmclassifier.py
+++ src/multilibsvmclassifier.py
@@ -13,19 +13,29 @@ class Fil():
 	def extract(self, image):
 		return filterer.filterImageMultiParam(image, self.p, self.fs1, self.fs2)
 
+# lambda je od 2 do 12 (pise da moze biti od 2 do 1/5 velicine slike)
+# Orijentacije... pa samo broj varirati
+# faza... mozda i tu samo broj varirati
+# gamma >0 do 1
+
 # Samo u petlji stvarati filtere i instancirati klasifikator
-lambdaSet = [2.5, 4, 5.6568, 8, 11.3137, 16]
+lambdaSets = [[2.5, 4, 5.6568, 8, 11.3137, 16], [2.5, 5, 10], [4, 8, 32]]
+gammas = [0.5, 0.7, 1]
+bandwidths = [0.5, 1.5, math.pi]
 orientationNum = 8
-gamma = 0.5
-bandwidth = math.pi
-_filterSet1 = []
-_filterSet2 = []
-for Lambda in lambdaSet:
-	for n in xrange(0, orientationNum):
-		_filterSet1.append(gabor.gaborFilterSimplified(Lambda, math.pi/8 * n, 0, bandwidth, gamma))
-		_filterSet2.append(gabor.gaborFilterSimplified(Lambda, math.pi/8 * n, math.pi/2, bandwidth, gamma))
 
+for lambdaSet in lambdaSets:
+	for gamma in gammas:
+		for bandwidth in bandwidths:
+			_filterSet1 = []
+			_filterSet2 = []
+			for Lambda in lambdaSet:
+				for n in xrange(0, orientationNum):
+					# gaborFilterSimplified(Lambda, theta, psi, bandwidth, gamma)
+					_filterSet1.append(gabor.gaborFilterSimplified(Lambda, math.pi/orientationNum * n, 0, bandwidth, gamma))
+					_filterSet2.append(gabor.gaborFilterSimplified(Lambda, math.pi/orientationNum * n, math.pi/2, bandwidth, gamma))
 
-fil = Fil(8, _filterSet1, _filterSet2)
-instance = libsvmclassifier.LibSVMclassifier(fil.extract)
-instance.name = "libsvm-l:"+str(lambdaSet)+"-g:"+str(gamma)+"-b:"+str(bandwidth)+"-on:"+str(orientationNum)
+			fil = Fil(8, _filterSet1, _filterSet2)
+			instance = libsvmclassifier.LibSVMclassifier(fil.extract)
+			instance.name = "libsvm-l:"+str(lambdaSet)+"-g:"+str(gamma)+"-b:"+str(bandwidth)+"-on:"+str(orientationNum)
+
